2c1be58379e7026ac4a50e121482eb876d847787
gprachan <70263211+gprachan@users.noreply.github.com>
2020-12-08 03:14:30 -0800

    Task Completed

--- app/src/main/java/com/example/firstkotlinapp/DiceGame.kt
+++ app/src/main/java/com/example/firstkotlinapp/DiceGame.kt
@@ -2,10 +2,32 @@ package com.example.firstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class DiceGame : AppCompatActivity() {
+    private lateinit var diceNumber: TextView
+    private lateinit var rollDice: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dice_game)
+        uiInitialize()
+        onRollDiceClick()
+    }
+
+    private fun onRollDiceClick() {
+        rollDice.setOnClickListener {
+            diceNumber.text = roll().toString()
+        }
+    }
+
+    private fun roll(): Int {
+        return (1..6).random()
+    }
+
+    private fun uiInitialize() {
+        diceNumber = findViewById(R.id.diceNumber)
+        rollDice = findViewById(R.id.rollDice)
     }
 }
--- app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.example.firstkotlinapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        main()
     }
 
-    private fun main() {
-        println("Happy Birthday!")
-        println("Jhansi\n")
-        println("You are 25!")
+    fun playGame(view: View) {
+        startActivity(Intent(this, DiceGame::class.java))
     }
+
 }
