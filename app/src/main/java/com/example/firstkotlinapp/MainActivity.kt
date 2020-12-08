9d1735714f374c5f245dc4a1bda250154a743a99
gprachan <70263211+gprachan@users.noreply.github.com>
2020-12-07 23:58:31 -0800

    Task Completed

--- app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        main()
+    }
+
+    private fun main() {
+        println("Happy Birthday!")
+        println("Jhansi\n")
+        println("You are 25!")
     }
 }
