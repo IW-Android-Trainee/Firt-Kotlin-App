6f98b3ddc9f5b1e71f2dd69c4fcb4cb1cc6408d1
gprachan <70263211+gprachan@users.noreply.github.com>
2020-12-09 12:11:47 +0545

    complete learning of array

--- app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.firstkotlinapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView as alias
+
+/*We cal type cast the imports using as keyword
+as keyword is also used for type cast*/
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: alias
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/firstkotlinapp/TestOne.kt
+++ app/src/main/java/com/example/firstkotlinapp/TestOne.kt
@@ -2,10 +2,19 @@ package com.example.firstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class TestOne : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_one)
+
+        val textView: TextView = findViewById(R.id.textView)
+
+        val myArray = Array(5) { value ->
+            value + 1
+        }
+        myArray.forEach { textView.append(it.toString() + "\n") }
+
     }
 }
